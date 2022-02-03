9811fac829846b5f9b6b29f59d97276297e724d5
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-03 17:07:58 +0300

    Added code to exercise08

---
+++ exercise08/Program.cs
@@ -0,0 +1,37 @@
+﻿//Показать четные числа от 1 до N
+
+Console.Write("Enter a random whole number: ");
+string numberStr = Console.ReadLine();
+int numberN = int.Parse(numberStr);
+int n = 1;
+
+while(n <= numberN)
+{
+    if
+    {
+        n % 2 != 0
+        Console.Write("Odd numbers are: ")
+        Console.Write(n + "...");
+    }
+    else    
+    {
+        n++;          
+    }
+    
+}
+else
+{
+    n++;
+}
+
+/*
+Console.WriteLine("Enter number N: ");
+string numbers = Console.ReadLine();
+int number = int.Parse(numbers);
+int numberNeg = number * -1;
+while (numberNeg <= number)
+{
+    Console.Write(numberNeg + " ");
+    numberNeg++;
+}
+*/
