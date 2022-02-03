60604541bc82d376d4858d74dbb6adb71039b329
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-03 07:27:30 +0300

    Added code to exercise02

---
+++ exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿//Даны два числа. Показать большее и меньшее число
+
+int numberOne = 1399;
+int numberTwo = 777;
+int min = numberOne;
+int max = 0;
+
+if (numberTwo < min) 
+{
+    min = numberTwo; 
+    max = numberOne;
+}
+else 
+{
+    max = numberTwo;
+} 
+Console.WriteLine(min + " is lower than " + max);
+
