63a051587d3d3c2d9a34805e36b900bc9cab9597
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-03 16:16:14 +0300

    Added code to exercise06

---
+++ exercise06/Program.cs
@@ -0,0 +1,15 @@
+﻿//Выяснить является ли число чётным
+
+int number = new Random().Next(999);
+
+if(number % 2 != 0)
+{
+    Console.WriteLine("The number " + number + " is odd.");
+}
+else
+{
+    Console.WriteLine("The number " + number + " is even.");
+}
+
+
+
