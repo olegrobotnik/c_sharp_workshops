e3149494f698b67a6b6ade05303a00207369a5ce
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-03 11:44:51 +0300

    Added code to exercise04

---
+++ exercise04/Program.cs
@@ -0,0 +1,13 @@
+﻿//Найти максимальное из трех чисел
+
+int numberOne = 1787;
+int numberTwo = 7777;
+int numberThree = 8787;
+
+int max = numberOne;
+
+if(numberTwo > max) max = numberTwo;
+if(numberThree > max) max = numberThree;
+
+Console.WriteLine("The biggest number is " + max);
+
