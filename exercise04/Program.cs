ff4c2683972f86dfefd80d3c2f42d48fc52627eb
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-03 13:27:04 +0300

    Added another version of code to exercise04

--- exercise04/Program.cs
+++ exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Найти максимальное из трех чисел
 
+/*
 int numberOne = 1787;
 int numberTwo = 7777;
 int numberThree = 8787;
@@ -10,4 +11,23 @@ if(numberTwo > max) max = numberTwo;
 if(numberThree > max) max = numberThree;
 
 Console.WriteLine("The biggest number is " + max);
+*/
 
+Console.Write("Enter number one: ");
+string numOneStr = Console.ReadLine();
+int numberOne = int.Parse(numOneStr);
+
+Console.Write("Enter number two: ");
+string numTwoStr = Console.ReadLine();
+int numberTwo = int.Parse(numTwoStr);
+
+Console.Write("Enter number two: ");
+string numThreeStr = Console.ReadLine();
+int numberThree = int.Parse(numThreeStr);
+
+int max = numberOne;
+
+if (numberTwo > max) max = numberTwo;
+if (numberThree > max) max = numberThree;
+
+Console.WriteLine("The biggest number is " + max);
