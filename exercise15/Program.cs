d8e8e8e2db6ffbf94a666e36e20bc8b6355fd338
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-09 17:03:00 +0300

    Formatted code in exercise15

---
+++ exercise15/Program.cs
@@ -0,0 +1,37 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int nmbr = 161;
+
+Multiplicity(nmbr);
+
+void Multiplicity(int nmbr) // Multiplicity of the number method 
+{
+    if (nmbr == 0)
+    
+        Console.WriteLine("The number is zero.");             
+    
+    else
+    {
+        if (nmbr % 7 == 0 && nmbr % 23 == 0)
+        {
+            Console.WriteLine($"The {nmbr} is multiple of 7 and 23.");    
+        }
+        else if (nmbr % 7 == 0)
+        {
+            Console.WriteLine($"The {nmbr} is multiple of 7."); 
+        }
+        else if (nmbr % 23 == 0)
+        {
+            Console.WriteLine($"The {nmbr} is multiple of 23.");   
+        }
+        else if (nmbr % 7 != 0)
+        {
+            Console.WriteLine($"The {nmbr} is non-multiple of 7.");
+        }      
+       
+        else if (nmbr % 23 != 0)
+        {
+            Console.WriteLine($"The {nmbr} is non-multiple of 23.");
+        }    
+    }
+}
