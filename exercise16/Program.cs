07f43d0a554af372049d52c3be8061572db5517f
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-09 17:03:44 +0300

    Formatted code in exercise16

---
+++ exercise16/Program.cs
@@ -0,0 +1,52 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int wkday;
+
+Console.Write("Enter a weekday number from 1 till 7): ");
+string value = Console.ReadLine();
+
+bool success = int.TryParse(value, out wkday);
+if (success)
+{
+    if (wkday >= 1 && wkday <= 7)
+    {
+        if (wkday == 6)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("It's Saturday!");
+            Console.ResetColor();
+        }
+        else if (wkday == 7)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("It's Sunday!");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine("It is a business day.");
+        }
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.DarkRed;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"{value} is not a weekday number!");
+        Console.ResetColor();
+    }
+}
+else
+{
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"{value} is not a weekday number!");
+    Console.ResetColor();
+}
+
+
+
+
+
+
