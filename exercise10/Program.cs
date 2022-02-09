7a62dca28ad5cd62f33d458112aefa9998bf1469
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-09 17:18:16 +0300

    Formatted code in exercise10

--- exercise10/Program.cs
+++ exercise10/Program.cs
@@ -1,12 +1,22 @@
 ﻿// Показать вторую цифру трёхзначного числа
 
-// int GetRandomNumber() // Added a function which returns a random number from 100 to 999
-// {
-//     int randomNumber = new Random().Next(100, 1000);
-//     return randomNumber;
-// }
+int rndmNmbr = new Random().Next(100, 1000); //This is the variant with int variables and division with remainder
+int dig1 = rndmNmbr / 10;
+int dig2 = dig1 % 10;
 
-/* int randomNumber = new Random().Next(100, 1000); //This is the variant with a string and a char variables
+Console.Write("The number is ");
+Console.ForegroundColor = ConsoleColor.Green; //Colored output
+Console.WriteLine(rndmNmbr);
+Console.ResetColor();
+
+Console.Write("The second digit is ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(dig2);
+Console.ResetColor();
+
+
+/* //This is the option with a string and a char variables
+int randomNumber = new Random().Next(100, 1000); 
 string numberStr = randomNumber.ToString("G");
 char two = numberStr[1];
 
@@ -21,18 +31,5 @@ Console.ResetColor();
 Console.Write("The second digit is ");
 Console.ForegroundColor = ConsoleColor.Magenta;
 Console.WriteLine(two);
-Console.ResetColor(); */
-
-int rndmNmbr = new Random().Next(100, 1000); //This is the variant with int variables and division with remainder
-int dig1 = rndmNmbr / 10;
-int dig2 = dig1 % 10;
-
-Console.Write("The number is ");
-Console.ForegroundColor = ConsoleColor.Green; //Colored output
-Console.WriteLine(rndmNmbr);
-Console.ResetColor();
-
-Console.Write("The second digit is ");
-Console.ForegroundColor = ConsoleColor.Magenta;
-Console.WriteLine(dig2);
-Console.ResetColor();
+Console.ResetColor(); 
+*/
