55990fd0dea91893a5e07aab27e9e8195d236261
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-02-09 17:12:57 +0300

    Formatted code in exercise14

--- exercise14/Program.cs
+++ exercise14/Program.cs
@@ -1,6 +1,48 @@
 ﻿//Найти третью цифру числа или сообщить, что её нет
 
 
+string InPut(string message)
+{
+    Console.Write(message);
+    string numStr = Console.ReadLine();
+    return numStr;    
+}
+
+string num1 = InPut("Enter the first number: ");
+
+int length = num1.Length;
+
+if (length > 2)
+{
+    char thrdChr =  num1[2];
+    Console.WriteLine($"The third digit of the number is {thrdChr}.");
+}
+else
+{
+    Console.WriteLine($"{num1} is not a three-digit number!");
+}
+
+/* Working completely math option
+Console.Write("Enter the number: ");
+int nmbr = int.Parse(Console.ReadLine());
+int nmbrPos = Math.Abs(nmbr);
+
+if (nmbrPos < 100)
+{
+    Console.WriteLine("Number is smaller than 3-digit");
+    
+}
+else
+{
+    while(nmbrPos > 999)
+    {
+        nmbrPos = nmbrPos / 10;
+    }
+    Console.WriteLine($"{nmbrPos % 10} is the third digit.");
+}
+*/
+
+
 /* Not working option with array
 int[] arry = new int[] { 100, 999, 101, 777, 7777, 77777, 888888, 9999999 };
 foreach (int elmnt in arry)
@@ -56,49 +98,3 @@ for (int i = 0; i < arry.Length; i++)
 
 }
 */
-
-/* Working completely math option
-Console.Write("Enter the number: ");
-int nmbr = int.Parse(Console.ReadLine());
-int nmbrPos = Math.Abs(nmbr);
-
-if (nmbrPos < 100)
-{
-    Console.WriteLine("Number is smaller than 3-digit");
-    
-}
-else
-{
-    while(nmbrPos > 999)
-    {
-        nmbrPos = nmbrPos / 10;
-    }
-    Console.WriteLine($"{nmbrPos % 10} is the third digit.");
-}
-*/
-
-string InPut(string message)
-{
-    Console.Write(message);
-    string numStr = Console.ReadLine();
-    //int A = int.Parse(num);
-    return numStr;    
-}
-
-string num1 = InPut("Enter the first number: ");
-//string num2 = InPut("Enter the second number: ");
-//string num3 = InPut("Enter the third number: ");
-
-int length = num1.Length;
-
-if (length > 2)
-{
-    char thrdChr =  num1[2];
-    Console.WriteLine($"The third digit of the number is {thrdChr}.");
-}
-else
-{
-    Console.WriteLine($"{num1} is not a three-digit number!");
-}
-
-
